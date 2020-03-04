be158a120a4e6dd55c441b6948a730c3cb3f5b3f
malonge <malonge11@gmail.com>
2020-03-04 17:34:41 -0500

    finished ordering and orienting and started script to write scaffolds in fasta format

---
+++ build_scaffolds.py
@@ -0,0 +1,99 @@
+import re
+import argparse
+from collections import defaultdict
+
+import pysam
+
+from ragoo_utilities.utilities import reverse_complement
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Build scaffolds from an 'orderings.bed' file")
+    parser.add_argument("orderings", metavar="<orderings.bed>", type=str, help="'orderings.bed' file produced by 'ragoo_scaffold.py'")
+    parser.add_argument("query", metavar="<query.fasta>", type=str, help="query fasta file to be scaffolded. must not be gzipped")
+    parser.add_argument("out_file", metavar="<ragoo.fasta>", type=str, help="output file name")
+    parser.add_argument("gap_size", metavar="100", default=100, type=int, help="gap size for chr0")
+    parser.add_argument("-C", default=False, action='store_true', help="write individual unplaced contigs instead of chr0")
+
+    args = parser.parse_args()
+    orderings_file = args.orderings
+    query_file = args.query
+    out_file = args.out_file
+    chr0_gap_size = args.gap_size
+    make_chr0 = not args.C
+
+    # Organize the orderings
+    orderings = defaultdict(list)
+    with open(orderings_file, "r") as f:
+        for line in f:
+            l = line.rstrip().split()
+            start, end, seq_type, header, strand = int(l[1]), int(l[2]), l[3], l[4], l[5]
+            orderings[l[0]].append((start, end, seq_type, header, strand))
+
+    # Sort the orderings and write the fasta output
+    out_fasta = open(out_file, "w")
+    curr_total = 0
+    curr_seq = []
+    x = pysam.FastaFile(query_file)
+    placed_q_seqs = set()
+    for i in orderings:
+        out_fasta.write(">" + i + "\n")
+        orderings[i] = sorted(orderings[i])
+        for j in orderings[i]:
+            q_header = j[3]
+            if j[2] == "s":
+                # This is a contig
+                placed_q_seqs.add(q_header)
+                if j[4] == "+":
+                    curr_seq.append(x.fetch(q_header))
+                else:
+                    curr_seq.append(reverse_complement(x.fetch(q_header)))
+                curr_total += j[1] - j[0]
+            else:
+                # This is a gap
+                assert j[2] == "g"
+                gap_len = j[1] - j[0]
+                curr_seq.append("N" * gap_len)
+                curr_total += gap_len
+
+            if curr_total >= 10 ** 7:  # Print out every 10Mbps
+                wrapped = re.findall(".{1,60}", "".join(curr_seq))
+                print(*wrapped[:-1], sep="\n", file=out_fasta)
+                curr_seq = [wrapped[-1]]
+                curr_total = len(wrapped[-1])
+
+    wrapped = re.findall(".{1,60}", "".join(curr_seq))
+    print(*wrapped, sep="\n", file=out_fasta)
+
+    all_q_seqs = set(x.references)
+    remaining_seqs = all_q_seqs - placed_q_seqs
+    if remaining_seqs:
+        if make_chr0:
+            out_fasta.write(">Chr0_RaGOO\n")
+            pad = "N" * chr0_gap_size
+            curr_total = 0
+            curr_seq = []
+            for i in remaining_seqs:
+                curr_seq.append(x.fetch(i))
+                curr_total += x.get_reference_length(i)
+
+                if curr_total >= 10 ** 7:  # Print out every 10Mbps
+                    wrapped = re.findall(".{1,60}", "pad".join(curr_seq))
+                    print(*wrapped[:-1], sep="\n", file=out_fasta)
+                    curr_seq = [wrapped[-1]]
+                    curr_total = len(wrapped[-1])
+            wrapped = re.findall(".{1,60}", "".join(curr_seq))
+            print(*wrapped, sep="\n", file=out_fasta)
+
+        else:
+            # Write unplaced contigs individually with original headers
+            for i in remaining_seqs:
+                wrapped = re.findall(".{1,60}", x.fetch(i))
+                out_fasta.write(">" + i + "\n")
+                print(*wrapped, sep="\n", file=out_fasta)
+
+    out_fasta.close()
+
+
+if __name__ == "__main__":
+    main()
--- ragoo_scaffold.py
+++ ragoo_scaffold.py
@@ -2,11 +2,64 @@ import os
 import argparse
 from collections import defaultdict
 
-from ragoo_utilities.utilities import log
+from ragoo_utilities.utilities import log, run
 from ragoo_utilities.Aligner import Minimap2Aligner
 from ragoo_utilities.Aligner import NucmerAligner
 from ragoo_utilities.AlignmentReader import AlignmentReader
 from ragoo_utilities.ContigAlignment import ContigAlignment
+
+
+def write_orderings(ordering_dict, ctg_dict, gap_dict, overwrite, out_path):
+    out_file = out_path + "orderings.bed"
+
+    # Check if the output file already exists
+    if os.path.isfile(out_file):
+        if not overwrite:
+            log("retaining pre-existing file: " + out_file)
+            return
+
+    # Proceed with writing the intermediate output
+    gap_id = 0
+    all_out_lines = []
+    for ref_header in ordering_dict:
+        pos = 0
+        new_ref_header = ref_header + "_RaGOO"
+        q_seqs = ordering_dict[ref_header]
+        gap_seqs = gap_dict[ref_header]
+
+        # Iterate through the query sequences for this reference header
+        # TODO give each gap a unique ID where query seq header goes
+        for i in range(len(q_seqs)):
+            out_line = []
+            q = q_seqs[i][2]
+            qlen = ctg_dict[q].query_len
+            strand = ctg_dict[q].orientation
+            gc, lc, oc = ctg_dict[q].grouping_confidence, ctg_dict[q].location_confidence, ctg_dict[q].orientation_confidence
+            out_line.append(new_ref_header)
+            out_line.append(str(pos))
+            pos += qlen
+            out_line.append(str(pos) + "\ts")
+            out_line.append(q)
+            out_line.append(strand)
+            out_line.append(str(gc))
+            out_line.append(str(lc))
+            out_line.append(str(oc))
+            all_out_lines.append("\t".join(out_line))
+
+            if i < len(gap_seqs):
+                out_line = []
+                # Print the gap line
+                out_line.append(new_ref_header)
+                out_line.append(str(pos))
+                pos += gap_seqs[i]
+                out_line.append(str(pos) + "\tg\t" + str(gap_id))
+                gap_id += 1
+                out_line.append("NA\tNA\tNA\tNA")
+                all_out_lines.append("\t".join(out_line))
+
+    log("writing: " + out_file)
+    with open(out_file, "w") as f:
+        f.write("\n".join(all_out_lines))
 
 
 def main():
@@ -21,11 +74,13 @@ def main():
     parser.add_argument("-j", metavar="<skip.txt>", type=str, default="", help="List of contigs to automatically leave unplaced")
     parser.add_argument("-g", metavar="INT", type=int, default=100, help="gap size for padding in pseudomolecules [100]")
     parser.add_argument("-l", metavar="INT", type=int, default=10000, help="minimum unique alignment length to use for scaffolding [10000]")
-    parser.add_argument("-q", metavar="INT", type=int, default=40, help="minimum mapping quality value for alignments. only pertains to minimap2 alignments [0]")
+    parser.add_argument("-q", metavar="INT", type=int, default=0, help="minimum mapping quality value for alignments. only pertains to minimap2 alignments [0]")
     parser.add_argument("-i", metavar="FLOAT", type=float, default=0.2, help="minimum grouping confidence score needed to be localized [0.2]")
+    parser.add_argument("-a", metavar="FLOAT", type=float, default=0.0, help="minimum location confidence score needed to be localized [0.0]")
+    parser.add_argument("-d", metavar="FLOAT", type=float, default=0.0, help="minimum orientation confidence score needed to be localized [0.0]")
     parser.add_argument("-C", action='store_true', default=False, help="write unplaced contigs individually instead of making a chr0")
     parser.add_argument("-r", action='store_true', default=False, help="infer gap pad sizes from the reference. '-g' is used when adjacent contigs overlap")
-    parser.add_argument("-w", action='store_true', default=False, help="overwrite pre-existing intermediate files.")
+    parser.add_argument("-w", action='store_true', default=False, help="overwrite pre-existing intermediate files. ragoo.fasta will always be overwritten")
 
     # Get the command line arguments and ensure all paths are absolute.
     args = parser.parse_args()
@@ -36,6 +91,8 @@ def main():
     min_q = args.q
     gap_size = args.g
     group_score_thresh = args.i
+    loc_score_thresh = args.a
+    orient_score_thresh = args.d
     make_chr0 = not args.C
     infer_gaps = args.r
     overwrite_files = args.w
@@ -56,7 +113,7 @@ def main():
     mm2_params = args.mm2_params
     nucmer_params = args.nucmer_params
 
-    # Get the skip and exclude
+    # Get the skip and exclude sets
     query_blacklist = set()
     if skip_file:
         with open(skip_file, "r") as f:
@@ -76,7 +133,7 @@ def main():
         os.mkdir(output_path)
 
     # Align the query to the reference
-    log("aligning the query to the reference")
+    log("Aligning the query to the reference")
     if aligner == "minimap2":
         al = Minimap2Aligner(reference_file, query_file, aligner_path, mm2_params, output_path + "query_against_ref", in_overwrite=overwrite_files)
     else:
@@ -84,7 +141,7 @@ def main():
     al.run_aligner()
 
     # Read and organize the alignments
-    log('reading alignments')
+    log('Reading alignments')
     ctg_alns = dict()
     aln_reader = AlignmentReader(output_path + "query_against_ref", aligner)
     for aln_line in aln_reader.parse_alignments():
@@ -97,9 +154,9 @@ def main():
                 ctg_alns[aln_line.query_header] = ctg_alns[aln_line.query_header].add_alignment(aln_line.ref_header, aln_line.ref_len, aln_line.ref_start, aln_line.ref_end, aln_line.query_start, aln_line.query_end, aln_line.strand, aln_line.mapq)
 
     # Filter the alignments
-    log("filtering alignments")
+    log("Filtering alignments")
     if aligner == "minimap2":
-        log('alignments are from minimap2. removing alignments with mapq < %r.' % min_q)
+        log('Alignments are from minimap2. removing alignments with mapq < %r.' % min_q)
         for i in ctg_alns:
             ctg_alns[i] = ctg_alns[i].filter_mapq(min_q)
             if ctg_alns[i] is not None:
@@ -109,31 +166,71 @@ def main():
             ctg_alns[i] = ctg_alns[i].unique_anchor_filter(min_len)
 
     # Remove query sequences which have no more qualifying alignments
-    # TODO impose confidence score thresholds
     fltrd_ctg_alns = dict()
     for i in ctg_alns:
         if ctg_alns[i] is not None:
-            fltrd_ctg_alns[i] = ctg_alns[i]
+            if all([
+                ctg_alns[i].grouping_confidence > group_score_thresh,
+                ctg_alns[i].location_confidence > loc_score_thresh,
+                ctg_alns[i].orientation_confidence > orient_score_thresh
+            ]):
+                fltrd_ctg_alns[i] = ctg_alns[i]
 
     # For each reference sequence which has at least one assigned query sequence, get the list of
     # all query sequences assigned to that reference sequence.
-    all_mapped_ref_seqs = defaultdict(list)
+    log("Ordering and orienting query sequences")
+    mapped_ref_seqs = defaultdict(list)
     for i in fltrd_ctg_alns:
         best_ref = fltrd_ctg_alns[i].best_ref_header
         ref_start, ref_end = fltrd_ctg_alns[i].get_best_ref_pos()
-        all_mapped_ref_seqs[best_ref].append(ref_start, ref_end, i)
+        mapped_ref_seqs[best_ref].append((ref_start, ref_end, i))
 
-    # Sort the query sequences for each reference sequence
-    # TODO make a new dict where ref seq is key, and list is the gap sizes to put between the query seqs
+    # Sort the query sequences for each reference sequence and define the padding sizes between adjacent query seqs
     pads_sizes = dict()
-    for i in all_mapped_ref_seqs:
-        all_mapped_ref_seqs[i] = sorted(all_mapped_ref_seqs[i])
+    for i in mapped_ref_seqs:
+        mapped_ref_seqs[i] = sorted(mapped_ref_seqs[i])
         if infer_gaps:
-            # TODO
-            pass
-        else:
-            pads_sizes[i] = [gap_size for i in range(len(all_mapped_ref_seqs[i])-1)]
+            # Infer the gap sizes between adjacent query seqs
+            pads_sizes[i] = []
+            for j in range(1, len(mapped_ref_seqs[i])):
+                left_ctg = mapped_ref_seqs[i][j-1][2]
+                left_min, left_max = fltrd_ctg_alns[left_ctg].get_best_ref_flanks()
+                right_ctg = mapped_ref_seqs[i][j][2]
+                right_min, right_max = fltrd_ctg_alns[right_ctg].get_best_ref_flanks()
 
+                # If the contigs overlap, revert to the fixed pre-defined gap size
+                if right_min - left_max >= 0:
+                    pads_sizes[i].append(right_min - left_max)
+                else:
+                    pads_sizes[i].append(gap_size)
+        else:
+            pads_sizes[i] = [gap_size for i in range(len(mapped_ref_seqs[i])-1)]
+
+    # Write the intermediate output file
+    write_orderings(mapped_ref_seqs, fltrd_ctg_alns, pads_sizes, overwrite_files, output_path)
+
+    # Write the scaffolds
+    log("Writing scaffolds")
+    if make_chr0:
+        cmd = [
+            "python3",
+            "build_scaffolds.py",
+            output_path + "orderings.bed",
+            query_file,
+            output_path + "ragoo.fasta",
+            str(gap_size)
+        ]
+    else:
+        cmd = [
+            "python3",
+            "build_scaffolds.py",
+            "-C",
+            output_path + "orderings.bed",
+            query_file,
+            output_path + "ragoo.fasta",
+            str(gap_size)
+        ]
+    run(" ".join(cmd))
 
 
 if __name__ == "__main__":
