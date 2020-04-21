81e7d9be1e9719b069ded0393b6f50eeb9e0e4ff
malonge <malonge11@gmail.com>
2020-04-21 17:09:53 -0400

    added gff update script. needs to be tested

--- ragoo_update_gff.py
+++ ragoo_update_gff.py
@@ -12,7 +12,8 @@ def sub_update(gff_file, bed_file):
     trans = defaultdict(IntervalTree)
     with open(bed_file, "r") as f:
         for line in f:
-            oh, start, end, stype, nh, strand = line.rstrip().split("\t")
+            fields = line.rstrip().split("\t")
+            oh, start, end, stype, nh, strand = fields[0], int(fields[1]), int(fields[2]), fields[3], fields[4], fields[5]
             if not strand == "+":
                 raise ValueError("The placement BED file is not formatted correctly. All strand should be positive.")
             if not stype == "s":
@@ -29,6 +30,10 @@ def sub_update(gff_file, bed_file):
                 fields = line.split("\t")
                 h, s, e = fields[0], int(fields[3]), int(fields[4])
                 s -= 1  # Keep everything zero-indexed
+
+                if h not in trans:
+                    raise ValueError("Inconsistent input files.")
+
                 ovlps = trans[h][s:e]
                 if len(ovlps) > 1:
                     raise ValueError(
@@ -48,14 +53,55 @@ def sub_update(gff_file, bed_file):
 
 
 def sup_update(gff_file, bed_file):
-    raise NotImplementedError()
+    # Make a dictionary associating each original sequence with the destination sequence
+    trans = {}
+    strands = {}
+    seq_lens = {}
+    with open(bed_file, "r") as f:
+        for line in f:
+            fields = line.rstrip().split("\t")
+            nh, start, end, stype, oh, strand = fields[0], int(fields[1]), int(fields[2]), fields[3], fields[4], fields[5]
+            trans[oh] = (start, end, nh)
+            strands[oh] = strand
+            seq_lens[oh] = end - start
+
+    # Iterate through the gff intervals and update them according to trans
+    with open(gff_file, "r") as f:
+        for line in f:
+            line = line.rstrip()
+            if line.startswith("#"):
+                print(line)  # Print this comment line
+            else:
+                fields = line.split("\t")
+                h, s, e, st = fields[0], int(fields[3]), int(fields[4]), fields[6]
+                s -= 1  # Keep everything zero-indexed
+
+                if h not in trans:
+                    raise ValueError("Inconsistent input files.")
+
+                # Check if the original sequence has been reverse complemented
+                if strands[h] == "-":
+                    l = seq_lens[h]
+                    s, e = l-e, l-s
+                    if st == "+":
+                        st = "-"
+                    else:
+                        st = "+"
+
+                new_s = trans[h][0] + s
+                new_e = trans[h][0] + e
+                fields[0] = trans[h][2]
+                fields[3] = str(new_s + 1)  # back to one-based indexing for gff format
+                fields[4] = str(new_e)
+                fields[6] = st
+                print("\t".join(fields))
 
 
 def main():
     parser = argparse.ArgumentParser(description="Update gff invervals given a 'placement' BED file")
     parser.add_argument("gff", metavar="<genes.gff>", type=str, help="gff file. must not be gzipped")
     parser.add_argument("bed", metavar="<placement.bed>", type=str, help="placement BED file")
-    parser.add_arguments("-c", action="store_true", default=False, type=str, help="placement BED file")
+    parser.add_argument("-c", action="store_true", default=False, type=str, help="placement BED file")
 
     args = parser.parse_args()
     gff_file = os.path.abspath(args.gff)
