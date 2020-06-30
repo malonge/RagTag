e42ee91e2268cc937bff65eb3be87941f60653c8
malonge <malonge11@gmail.com>
2020-06-30 14:16:09 -0400

    updated setup.py for PyPI upload

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ import glob
 
 from ragtag_utilities.utilities import get_ragtag_version
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 scripts = glob.glob("*.p*")
 
 version = get_ragtag_version()[1:]
@@ -12,16 +15,25 @@ version = get_ragtag_version()[1:]
 setup(
     name='RagTag',
     version=version,
-    description='A tool to order and orient genome assembly contigs via mappings to a reference genome.',
     author='Michael Alonge',
     author_email='malonge11@gmail.com',
+    description='Fast reference-guided genome assembly scaffolding.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/malonge/RagTag",
     packages=['ragtag_utilities'],
     package_dir={'ragtag_utilities': 'ragtag_utilities/'},
+    license="MIT",
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+    ],
     install_requires=[
               'intervaltree',
               'numpy',
               'pysam',
           ],
+    python_requires='>=3.6',
     scripts=scripts,
     zip_safe=True
 )
