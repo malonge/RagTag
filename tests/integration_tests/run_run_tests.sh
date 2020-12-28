afe84cbcf8197caedcd2a53f5bd51ae7478b4861
malonge <malonge11@gmail.com>
2020-12-28 11:22:51 -0500

    updated A. thaliana reference file in tests

--- tests/integration_tests/run_run_tests.sh
+++ tests/integration_tests/run_run_tests.sh
@@ -3,7 +3,7 @@
 bash run_tests.sh ~/Projects/Reference_Genomes/Tomato/SL4.0.genome.fasta \
     ~/Projects/ragtag_workspace/tomato_test_env/Brandywine.assembly.polished.fasta \
     ~/Projects/ragtag_workspace/tomato_test_env/brandywine_ctg_genes.gff \
-    ~/Projects/Reference_Genomes/Arabidopsis/GCF_000001735.3_TAIR10_genomic.fna \
+    ~/Projects/Reference_Genomes/Arabidopsis/TAIR10_chr_all.fasta \
     ~/Projects/ragtag_workspace/ara_test_env/Arabidopsis.MaSuRCA.3.4.0.fasta \
     ~/Projects/ragtag_workspace/ara_test_env/Arabidopsis.MaSuRCA.3.4.0.genes.gff \
     ~/Projects/ragtag_workspace/ecoli_tests/GCF_000008865.2_ASM886v2_genomic.fna \
