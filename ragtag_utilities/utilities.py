4d3cf6d153d30d0a507a68a04618d98eaf5c728e
malonge <malonge11@gmail.com>
2021-01-27 14:36:19 -0500

    initiated new branch for v1.1.0 updates

--- ragtag_utilities/utilities.py
+++ ragtag_utilities/utilities.py
@@ -36,7 +36,7 @@ complements = str.maketrans("ACGTNURYSWKMBVDHacgtnuryswkmbvdh", "TGCANAYRSWMKVBH
 
 
 def get_ragtag_version():
-    return 'v1.0.2'
+    return 'v1.1.0'
 
 
 def reverse_complement(seq):
