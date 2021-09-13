fd8d12b372841fa470b53bc1769c789777d72970
malonge <malonge11@gmail.com>
2021-09-12 21:23:39 -0400

    updated version number

--- ragtag_utilities/utilities.py
+++ ragtag_utilities/utilities.py
@@ -36,7 +36,7 @@ complements = str.maketrans("ACGTNURYSWKMBVDHacgtnuryswkmbvdh", "TGCANAYRSWMKVBH
 
 
 def get_ragtag_version():
-    return 'v2.0.1'
+    return 'v2.1.0'
 
 
 def reverse_complement(seq):
